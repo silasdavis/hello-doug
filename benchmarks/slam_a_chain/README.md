4cce002ba79ec7c3bd5d5898f375b6003a694097
David Braun <David@NodeGuy.com>
2017-04-19 01:17:22 -0400

    Update slam_a_chain to work with Monax 0.16.

--- benchmarks/slam_a_chain/README.md
+++ benchmarks/slam_a_chain/README.md
@@ -1,3 +1,21 @@
-an app.js that plugs into a chain and slams it with (a) sends from one account
-to another; and then (b) a bunch of sets in a simple get/set contract. Probably
-on the order of 100k sends and sets. And then displays the results at the end.
+# Slam a Chain
+
+Step right up folks and get ready to slam a chain, a blockchain that is!  This application creates a test blockchain and slams it with 100,000 `sends` and 100,000 contract calls in order to benchmark performance.  Transactions are sent in parallel until Burrow responds with an error, at which point the number of simultaneous transactions is throttled.
+
+## Prerequisites
+
+* [Git](https://git-scm.com/)
+* [Monax](https://monax.io/) version 0.16
+* [Node.js](https://nodejs.org/) version 6 or higher
+
+## Installation
+
+`npm install`
+
+## Use
+
+`npm start`
+
+## Known Issues
+
+The `sends` test fails to complete, complaining of `Error broadcasting transaction: Unknown error returned: Duplicate transaction (ignored)` errors.
--- benchmarks/slam_a_chain/lib/index.js
+++ benchmarks/slam_a_chain/lib/index.js
@@ -1,154 +1,228 @@
 'use strict'
 
 const childProcess = require('mz/child_process')
-const ErisDb = require('eris-db')
-const erisContracts = require('eris-contracts')
-const F = require('fairmont')
+const httpRequest = require('request-promise')
+const legacyDb = require('@monax/legacy-db')
+const legacyContracts = require('@monax/legacy-contracts')
 const fs = require('mz/fs')
 const humanizeDuration = require('humanize-duration')
-const I = require('./iterators')
 const numeral = require('numeral')
-const Promise = require('bluebird')
-const R = require('ramda')
-const slam = require('./slam')
+const os = require('os')
+const path = require('path')
 const Solidity = require('solc')
-const untildify = require('untildify')
 const url = require('url')
 
-const exec = R.composeP(R.trim, R.head, childProcess.exec)
+const exec = (command) =>
+  childProcess.exec(command).then(([stdout]) => stdout.trim())
 
-const privateValidator = () =>
-  fs.readFile(untildify('~/.eris/chains/default/priv_validator.json'))
-    .then(JSON.parse)
+const blockchainName = 'slams'
+
+const initDir = () =>
+  path.join(
+    os.homedir(),
+    `.monax/chains/${blockchainName}/${blockchainName.toLowerCase()}_full_000`
+  )
 
 const dockerMachineIp = () =>
-  exec('docker-machine ip').catch(() => 'localhost')
+  exec('docker-machine ip $(docker-machine active)').catch(() => 'localhost')
 
-const blockchainUrl = (name) => {
-  const blockchainPort = () =>
-    exec(`
-      id=$(eris chains inspect ${name} Id)
-      docker inspect --format='{{(index (index .NetworkSettings.Ports "1337` +
-        `/tcp") 0).HostPort}}' $id
-    `)
+const blockchainUrl = (hostname, port) =>
+  url.format({
+    protocol: 'http:',
+    slashes: true,
+    hostname,
+    port,
+    pathname: '/rpc'
+  })
 
-  return Promise.all([dockerMachineIp(), blockchainPort()])
-    .spread((hostname, port) => ({
-      protocol: 'http:',
-      slashes: true,
-      hostname,
-      port,
-      pathname: '/rpc'
+const blockchainIsAvailable = (url) =>
+  new Promise((resolve) => {
+    const poll = () => {
+      httpRequest(url).then(
+        resolve,
+        (reason) => {
+          if (reason.name === 'RequestError') {
+            setTimeout(poll, 100)
+          } else {
+            resolve()
+          }
+        }
+      )
+    }
+
+    poll()
+  })
+
+const newBlockchainPort = () => {
+  const dir = initDir()
+
+  return exec(`
+    monax chains rm --data --dir --force ${blockchainName}
+    monax chains make --unsafe ${blockchainName}
+    monax chains start --init-dir ${dir} --publish ${blockchainName}
+    id=$(monax chains inspect ${blockchainName} Id)
+    docker inspect --format=\
+'{{(index (index .NetworkSettings.Ports "1337/tcp") 0).HostPort}}' $id
+  `, {env: Object.assign({}, process.env, {MONAX_PULL_APPROVE: true})})
+}
+
+const newBlockchain = () => {
+  console.log(`Creating test blockchain.`)
+
+  return Promise.all([
+    dockerMachineIp(),
+    newBlockchainPort()
+  ]).then(([ipAddress, port]) =>
+    Promise.all([
+      fs.readFile(path.join(initDir(), 'priv_validator.json'))
+        .then(JSON.parse),
+
+      blockchainUrl(ipAddress, port)
+    ]).then(([validator, blockchainUrl]) => {
+      const blockchain = legacyDb.createInstance(url.format(blockchainUrl))
+
+      return blockchainIsAvailable(blockchainUrl)
+        .then(() => ({blockchain, blockchainUrl, validator}))
     })
   )
 }
 
-const blockchainIsAvailable = (erisDb) =>
-  I.poll(
-    () => Promise.fromCallback(
-      (callback) => erisDb.blockchain().getChainId(callback)
-    ),
-    I.intervalAsyncIterator(100)
-  ).then(F.wrap(erisDb))
-
-const newBlockchain = (name) =>
-  exec(`
-    eris chains rm --data --dir --file --force ${name}
-    eris chains new --publish ${name}
-  `, {env: R.assoc('ERIS_PULL_APPROVE', true, process.env)}).then(() =>
-    blockchainUrl(name).then((blockchainUrl) =>
-      blockchainIsAvailable(ErisDb.createInstance(url.format(blockchainUrl)))
-    )
-  )
-
-const blockchainName = 'slams'
-
 const source = `
-  contract SimpleStorage {
-      uint storedData;
+    contract SimpleStorage {
+        uint storedData;
 
-      function set(uint x) {
-          storedData = x;
-      }
+        function set(uint x) {
+            storedData = x;
+        }
 
-      function get() constant returns (uint retVal) {
-          return storedData;
-      }
-  }
+        function get() constant returns (uint) {
+            return storedData;
+        }
+    }
 `
 
 const compile = source =>
   Solidity.compile(source, 1).contracts
 
-const metric = (command, transactions, milliseconds) =>
-  `${Math.round(transactions / (milliseconds / 1000))} ${command}s per second`
+const metric = (name, transactions, duration) =>
+  transactions > 0
+    ? ` (${Math.round(transactions / (duration / 1000))} ` +
+      `${name}s per second)`
+    : ``
 
-Promise.all([newBlockchain(blockchainName), privateValidator()])
-  .spread((erisDb, validator) => {
-    const contractManager = erisContracts.newContractManagerDev(
-      erisDb._client._URL, {
-        address: validator.address,
-        pubKey: validator.pub_key,
-        privKey: validator.priv_key
-      }
-    )
+const slam = (count, {name, description, action}, start) => {
+  console.log(`\nRepeating ${numeral(count).format('0,0')} ` +
+    `times:\n${description}`)
 
-    const compiled = compile(source).SimpleStorage
-    const abi = JSON.parse(compiled.interface)
-    const bytecode = compiled.bytecode
-    const contractFactory = contractManager.newContractFactory(abi)
+  return new Promise((resolve, reject) => {
+    let progressTimeout
+    let successes = 0
+    let successTimeout
+    let throttled = false
 
-    Promise.fromCallback((callback) =>
-      contractFactory.new({data: bytecode}, callback)
-    ).then((contract) => {
+    const transact = () => {
+      setImmediate(() => {
+        action((error) => {
+          if (error) {
+            if (!throttled) {
+              throttled = true
+              console.log(`Received error, throttling transactions.`)
+            }
+          } else {
+            successes++
+            clearTimeout(successTimeout)
+
+            successTimeout = setTimeout(() => {
+              console.log(`Last successful transaction was over 5 seconds ` +
+                `ago, giving up.`)
+
+              clearTimeout(progressTimeout)
+              resolve(successes)
+            }, 5 * 1000)
+
+            if (successes >= count) {
+              resolve(successes)
+            } else if (throttled) {
+              // When we're throttled, start a new transaction only after a
+              // previous one completes successfully.
+              transact()
+            }
+          }
+        })
+
+        // Start as many transactions at once as we can before we're throttled.
+        if (!throttled) {
+          transact()
+        }
+      })
+    }
+
+    const reportProgress = () => {
+      progressTimeout = setTimeout(() => {
+        if (successes < count) {
+          reportProgress()
+        }
+
+        console.log(`${Math.floor(100 * successes / count)}%` +
+          `${metric(name, successes, Date.now() - start)}\n`)
+      }, 1000)
+    }
+
+    transact()
+    reportProgress()
+  }).then((successes) => {
+    const duration = Date.now() - start
+
+    console.log(`duration: ${humanizeDuration(duration)}` +
+      `${metric(name, successes, duration)}`)
+  })
+}
+
+newBlockchain(blockchainName).then(({blockchain, blockchainUrl, validator}) => {
+  const contractManager = legacyContracts.newContractManagerDev(blockchainUrl, {
+    address: validator.address,
+    pubKey: validator.pub_key,
+    privKey: validator.priv_key
+  })
+
+  const compiled = compile(source).SimpleStorage
+  const abi = JSON.parse(compiled.interface)
+  const bytecode = compiled.bytecode
+  const contractFactory = contractManager.newContractFactory(abi)
+
+  contractFactory.new({data: bytecode}, (error, contract) => {
+    if (error) {
+      throw error
+    } else {
       const privateKey = validator.priv_key[1]
       const destination = '0000000000000000000000000000000000000010'
       const amount = 1
       const count = 100000
 
-      const send = () =>
-        Promise.fromCallback((callback) =>
-          erisDb.txs().send(privateKey, destination, amount, null, callback)
-        )
+      const send = {
+        name: 'send',
 
-      const set = () =>
-        Promise.fromCallback((callback) =>
-          contract.set(42, callback)
-        )
+        description: `Send ${amount} ether
+    from ${validator.address}
+    to ${destination}.`,
 
-      const runSlam = (command, step, description) => {
-        console.log(`\nRepeating ${numeral(count).format('0,0')} times:\n${description}`)
-        const start = Date.now()
-
-        const progress = F.curry((total, value) => {
-          const metricString = value > 0
-            ? ` (${metric(command, value, Date.now() - start)})`
-            : ``
-
-          return `${Math.floor(100 * value / total)}%${metricString}\n`
-        })
-
-        const pipeline = F.pipe(
-          slam(count),
-          F.map(progress(count))
-        )
-
-        const status = I.iteratorToStream(pipeline(step))
-        status.pipe(process.stdout)
-
-        return I.streamToPromise(status).then(() => {
-          const duration = Date.now() - start
-
-          console.log(`duration: ${humanizeDuration(duration)} ` +
-            `(${metric(command, count, duration)})`)
-        })
+        action: (callback) =>
+          blockchain.txs().send(privateKey, destination, amount, null, callback)
       }
 
-      return runSlam('send', send, `Send ${amount} ether
-from ${validator.address}
-to ${destination}.`)
-        .then(() => {
-          return runSlam('set', set, 'Set a value in a contract.')
-        })
-    })
+      const set = {
+        name: 'set',
+        description: 'Set a value in a contract.',
+
+        action: (callback) =>
+          contract.set(42, callback)
+      }
+
+      slam(count, send, Date.now())
+        .then(() =>
+          slam(count, set, Date.now())
+        )
+        .catch(console.error)
+    }
   })
+})
